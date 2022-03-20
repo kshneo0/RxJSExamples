20944b9d7021affc6bfb93ee712d186310ceae7a
kshneo0 <kshneo@gmail.com>
2022-03-20 09:53:39 +0900

    11. 6 - Max and Min operators

--- src/10-testingrx.js
+++ src/10-testingrx.js
@@ -1,6 +1,11 @@
 import { of } from "rxjs";
 import { count } from "rxjs/operators";
-
+/*
 let all_nums = of(1, 7, 5, 10, 10, 20);
 let final_val = all_nums.pipe(count());
+final_val.subscribe((x) => console.log("The count is " + x));
+*/
+
+let all_nums = of(1, 6, 5, 10, 9, 20, 40);
+let final_val = all_nums.pipe(count((a) => a % 2 === 0));
 final_val.subscribe((x) => console.log("The count is " + x));
