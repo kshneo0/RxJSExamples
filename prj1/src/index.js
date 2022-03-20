3c34650480928dd38ee3f69afd33fa2f5c531e73
kshneo0 <kshneo@gmail.com>
2022-03-20 17:42:17 +0900

    32. 4 - Implementing a render function that displays the posts

--- prj1/src/index.js
+++ prj1/src/index.js
@@ -1,3 +1,4 @@
+import { first } from "rxjs";
 import { fromEvent } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
@@ -6,7 +7,26 @@ const button = document.querySelector("button");
 
 fromEvent(button, "click").subscribe(getPosts, (err) => console.error(err));
 
-
 function getPosts() {
-  
+  const userId = Math.round(Math.random() * 10);
+  ajax(
+    `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+  ).subscribe((res) => {
+    res && res.response && render(res);
+  });
+}
+
+function render(res) {
+  div.innerHTML = "";
+  const posts = res.response;
+  for (const post of posts) {
+    const article = document.createElement("article");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+    h1.textContent = post.title;
+    p.textContent = `id:${post.id} user-id:${post.userId}`;
+    article.appendChild(h1);
+    article.appendChild(p);
+    div.appendChild(article);
+  }
 }
